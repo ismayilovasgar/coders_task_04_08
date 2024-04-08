3855cdcd0c82f85ad3edfb36439b96b3f437248e
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2024-04-09 00:13:46 +0400

    2 ci versiya

--- app.js
+++ app.js
@@ -5,26 +5,27 @@ const slider_img = document.createElement("img");
 let i = 0;
 let images = [];
 
-//* ilk YOl sadece ilk sekil gec yuklenir
-// fetch("https://fakestoreapi.com/products?limit=10")
-//   .then((res) => res.json())
-//   .then((data) => {
-//     data.map((el) => {
-//       images.push(`${el.image}`);
-//     });
+// * ilk YOl sadece ilk sekil gec yuklenir
+fetch("https://fakestoreapi.com/products?limit=10")
+  .then((res) => res.json())
+  .then((data) => {
+    data.map((el) => {
+      images.push(`${el.image}`);
+    });
+  });
+slider_img.classList.add("slider-img");
+slider_img.src = images[0];
+img_box.append(slider_img);
+
+// const request = async () => {
+//   const response = await fetch("https://fakestoreapi.com/products?limit=20");
+//   const data = await response.json();
+//   await data.map((el) => {
+//     images.push(`${el.image}`);
 //   });
 
-const request = async () => {
-  const response = await fetch("https://fakestoreapi.com/products?limit=10");
-  const data = await response.json();
-  data.map((el) => {
-    images.push(`${el.image}`);
-  });
-  slider_img.classList.add("slider-img");
-  slider_img.src = images[0];
-  img_box.append(slider_img);
-};
-request();
+// };
+// request();
 
 prev__btn.addEventListener("click", () => {
   if (i <= 0) i = images.length;
